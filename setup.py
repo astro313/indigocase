7b4745373334bd6432472cff092e04ea8f015dc9
Daisy Leung <leungtszkuk@gmail.com>
2019-11-11 13:49:26 -0500

    setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from glob import glob
 from setuptools import find_packages
 from os.path import *
 
+
 setup(name='indigocase',
       version='1.0',
       description='Code base for Indigo case',
       url='http://github.com/astro313/indigocase',
       author='Daisy Leung',
       packages=find_packages('src'),
-      package_dir={'': 'src'},
+      package_dir={'indigocase': 'src'},
       install_requires=['markdown'],
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
       zip_safe=False)
