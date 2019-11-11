ec58cdb2219df6c7c984e300547bd50c9d62665d
Daisy Leung <leungtszkuk@gmail.com>
2019-11-11 17:23:27 -0500

    some test

--- indigocase/tests/bu_test.py
+++ indigocase/tests/bu_test.py
@@ -1,2 +1,19 @@
 from indigocase.src.parser import Parser
 from indigocase.src.config import Configurable
+import os
+
+
+def test_yaml_input_fields(yfname='config.yaml'):
+
+    ccc = Configurable(yfname)
+    setupParam = ccc.config_dict
+
+    dpath = setupParam['input']['dataPath']
+    trainfile = os.path.join(dpath, setupParam['input']['trainFile'])
+    testfile = os.path.join(dpath, setupParam['input']['testFile'])
+    truthfile = os.path.join(dpath, setupParam['input']['truthFile'])
+
+    assert os.path.isfile(trainfile)
+    assert os.path.isfile(testfile)
+    assert os.path.isfile(truthfile)
+
