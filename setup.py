0b6d38b6ec2d223745b329ddc89bebd96ad6f017
Daisy Leung <leungtszkuk@gmail.com>
2019-11-11 12:24:02 -0500

    travis

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from glob import glob
+from setuptools import find_packages
+
+setup(name='indigocase',
+      version='1.0',
+      description='Code base for Indigo case',
+      url='http://github.com/astro313/indigocase',
+      author='Daisy Leung',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=['markdown'],
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      zip_safe=False)
